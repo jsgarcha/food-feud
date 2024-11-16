7d905e2a811443361102cc2abe21275aef0d7b69
Jasminder Garcha <jasminder.garcha@gmail.com>
2024-11-16 12:54:57 -0500

    Scratch.

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-# Imports
 import pandas as pd
 import random
-from sklearn.model_selection import train_test_split
+import streamlit as st
 
-# Load the datasets
-df_restaurants = pd.read_csv('data/restaurants.csv')
-df_menus = pd.read_csv('data/restaurant-menus.csv')
-
-asked_restaurants = set()
-preferred_restaurants = []
-preference_number = 20
-
-# Loops until it finds a restaurant that wasn't asked about before
-def get_restaurant():
-    while True:
-        restaurant = random.choice(df_restaurants['name'])
-        if restaurant not in asked_restaurants:
-            asked_restaurants.add(restaurant)
-            return restaurant
-
-#Check if user prefers restaurant ("Do you like X?") If so, add to preference. If not, ask again.
-while len(preferred_restaurants) <= preference_number:
-    restaurant = get_restaurant()
-    prefers = input('Do you like ' + restaurant + '? (y/n)')
-    if prefers == 'y':
-        preferred_restaurants.append(restaurant)
+st.markdown(
+    "<h1 style='text-align: center'><a href='https://github.com/jsgarcha/food-feud'>Food Feud</a></h1>"
+    "<h3 style='text-align: center'>Start by taking our survey of eating establishments whose food you enjoy.</h1>",
+    unsafe_allow_html=True
+)
