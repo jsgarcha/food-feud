4437d6d813da6f501c9de5a2ac45c39e42de539a
Jasminder Garcha <jasminder.garcha@gmail.com>
2024-11-19 00:04:18 -0500

    Updated building up preferences.

--- main.py
+++ main.py
@@ -7,36 +7,35 @@ st.markdown("<h1 style='text-align: center'><a href='https://github.com/jsgarcha
 RESTAURANT_SURVEY_STAGE = 1
 RECIPE_GENERATION_STAGE = 2
 
-PREFERRED_NUMBER = 20
+PREFER_NUMBER = 20
 
 if 'stage' not in st.session_state:
     st.session_state.stage = RESTAURANT_SURVEY_STAGE # Start stage
 
-if "preferred" not in st.session_state:
-    st.session_state.preferred = []
+if "prefer" not in st.session_state:
+    st.session_state.prefer = []
 
-if "not_preferred" not in st.session_state:
-    st.session_state.not_preferred = []
+if "not_prefer" not in st.session_state:
+    st.session_state.not_prefer = []
 
-if "preferred_count" not in st.session_state:
-    st.session_state.preferred_count = PREFERRED_NUMBER
+if "prefer_count" not in st.session_state:
+    st.session_state.prefer_count = PREFER_NUMBER
 
-if "progress" not in st.session_state:
-    st.session_state.progress = 0
+if "survey_progress" not in st.session_state:
+    st.session_state.survey_progress = 0
 
 @st.cache_data
 def load_restaurant_data():
     return pd.read_csv('./data/restaurants.csv')
 
-def add_preferred(preferred): # Preference is a row in a DataFrame
-    st.session_state.preferred.append(preferred)
-    #st.session_state.not_preferred.remove(preferred)
-    if st.session_state.progress < 100:
-        st.session_state.progress += 100//PREFERRED_NUMBER
-        st.session_state.preferred_count -= 1
-        progress_bar.progress(st.session_state.progress, text=f"Select {st.session_state.preferred_count} more.")
-    df_preferred = pd.DataFrame(st.session_state.preferred)
-    st.dataframe(df_preferred)
+def add_prefer(prefer): # Preference is a row in a DataFrame
+    st.session_state.prefer.append(prefer) # Build up preferences!
+    #st.session_state.not_prefer.remove(prefer)
+    if st.session_state.survey_progress < 100:
+        st.session_state.survey_progress += 100//PREFER_NUMBER
+        st.session_state.prefer_count -= 1
+        survey_progress_bar.progress(st.session_state.survey_progress, text=f"Select {st.session_state.prefer_count} more.")
+    
 
 def display_restaurants(df_restaurants): 
     col1, col2 = st.columns(2) # Fixed 2 columns
@@ -44,8 +43,8 @@ def display_restaurants(df_restaurants):
         with col1:
             random_row = df_restaurants.sample()
             df_restaurants.drop(random_row.index, axis=0, inplace=True) # Remove sample from original, so isn't sampled twice (or more)
-            st.session_state.not_preferred.append(random_row) # Assume all not-clicked are not-prefers; maybe there's a trend to eventually see
-            st.button(random_row['name'].iloc[0], use_container_width=True, on_click=add_preferred, args=[random_row])
+            st.session_state.not_prefer.append(random_row) # Assume all not-clicked are not-prefers; maybe there's a trend to eventually see
+            st.button(random_row['name'].iloc[0], use_container_width=True, on_click=add_prefer, args=[random_row])
 
 df_restaurants = load_restaurant_data()
 
@@ -54,15 +53,16 @@ placeholder = st.empty()
 if st.session_state.stage == RESTAURANT_SURVEY_STAGE:
     with placeholder.container():
         st.markdown("<h3 style='text-align: center'>Start by taking our survey of eating establishments whose food you enjoy.</h1>", unsafe_allow_html=True)
-        progress_bar = st.progress(st.session_state.progress, text=f"Select {st.session_state.preferred_count} more.")
+        survey_progress_bar = st.progress(st.session_state.survey_progress, text=f"Select {st.session_state.prefer_count} more.")
         display_restaurants(df_restaurants)
         if st.button("More restaurants!", type='primary'):
             display_restaurants(df_restaurants)
 
-if st.session_state.preferred_count == 0:
+if st.session_state.prefer_count == 0:
     placeholder.empty()
     st.balloons()
     st.session_state.stage = RECIPE_GENERATION_STAGE
 
 if st.session_state.stage == RECIPE_GENERATION_STAGE:
-    st.write('RECIPE_GENERATION_STAGE')
+    df_restaurant_preferences = pd.concat(st.session_state.prefer)
+    st.dataframe(df_restaurant_preferences)
